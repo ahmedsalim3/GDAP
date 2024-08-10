ee5014c4fcc2455a8d8107f2ab62832c50249da1
Ahmed <a.hvshim@gmail.com>
2024-08-11 04:27:30 +0800

    Added main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import argparse
+from graph_composer import GraphComposer
+
+def main(disease_id, disease_name):
+    composer = GraphComposer(disease_id, disease_name)
+    composer.process_all(plot=True)
+
+if __name__ == "__main__":
+    hardcode_values = True  # Set to True for hardcoded values, False for command-line arguments
+
+    if hardcode_values:
+        disease_id = "EFO_0005741"
+        disease_name = "Infectious"
+    else:
+        parser = argparse.ArgumentParser(description='Process some diseases.')
+        parser.add_argument('disease_id', type=str, help='The ID of the disease')
+        parser.add_argument('disease_name', type=str, help='The name of the disease')
+        args = parser.parse_args()
+        disease_id = args.disease_id
+        disease_name = args.disease_name
+    
+    main(disease_id, disease_name)
