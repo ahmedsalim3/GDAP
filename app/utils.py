dfbc67b6b0612ba856ebe7985db795e1753546b7
ahmedsalim <realahmedsalim@gmail.com>
2024-09-24 22:53:48 +0800

    add functions to prepare and download graph-related files in memory

--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,10 @@ from src.open_targets.bigquery_fetcher import BigQueryClient, direct_scores, ind
 from src.open_targets.graphql_fetcher import GraphQLClient
 from src.ppi_data import PPIData
 import streamlit as st
+import networkx as nx
+import pandas as pd
+import numpy as np
+import io
      
 @st.cache_data
 def fetch_graphql_data(disease_id):
@@ -42,3 +46,64 @@ def init_values(key, value=None):
     """
     if key not in st.session_state:
         st.session_state[key] = value
+      
+
+
+def prepare_graph_files_in_memory(G, positive_edges, negative_edges):
+    # Prepare the files in memory
+    st.session_state.edgelist_buffer = io.BytesIO()
+    nx.write_edgelist(G, st.session_state.edgelist_buffer, data=True)  # graph.edgelist
+    st.session_state.edgelist_buffer.seek(0)
+    
+    st.session_state.graphml_buffer = io.BytesIO()
+    nx.write_graphml(G, st.session_state.graphml_buffer)  # graph.graphml
+    st.session_state.graphml_buffer.seek(0)
+    
+    edges = [(u, v) for u, v in G.edges()]
+    edges_df = pd.DataFrame(edges, columns=["source", "destination"])
+    st.session_state.edges_csv_buffer = io.BytesIO()
+    edges_df.to_csv(st.session_state.edges_csv_buffer, index=False)  # edges.csv
+    st.session_state.edges_csv_buffer.seek(0)
+    
+    st.session_state.positive_edges_buffer = io.BytesIO()
+    np.save(st.session_state.positive_edges_buffer, np.array(positive_edges)) # positive_edges.npy
+    st.session_state.positive_edges_buffer.seek(0)
+    
+    st.session_state.negative_edges_buffer = io.BytesIO()
+    np.save(st.session_state.negative_edges_buffer, np.array(negative_edges)) # negative_edges.npy
+    st.session_state.negative_edges_buffer.seek(0)  
+        
+        
+def download_graph_files():
+    st.download_button(
+        label="Download Edge List (edgelist.txt)",
+        data=st.session_state.edgelist_buffer,
+        file_name="graph.edgelist",
+        mime="text/plain"
+    )
+    st.download_button(
+        label="Download GraphML (graph.graphml)",
+        data=st.session_state.graphml_buffer,
+        file_name="graph.graphml",
+        mime="application/xml"
+    )
+    st.download_button(
+        label="Download Edges CSV (edges.csv)",
+        data=st.session_state.edges_csv_buffer,
+        file_name="edges.csv",
+        mime="text/csv"
+    )
+    st.download_button(
+        label="Download Positive Edges",
+        data=st.session_state.positive_edges_buffer,
+        file_name="positive_edges.npy",
+        mime="application/octet-stream"
+    )
+    st.download_button(
+        label="Download Negative Edges",
+        data=st.session_state.negative_edges_buffer,
+        file_name="negative_edges.npy",
+        mime="application/octet-stream"
+    )
+
+
