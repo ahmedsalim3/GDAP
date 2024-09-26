2f63094efacedabaa06ddbf54ff1c788f00b55bb
ahmedsalim <realahmedsalim@gmail.com>
2024-09-26 08:25:56 +0800

    added BaseNodeEmbedder class
    add functions to save vectors in gensim format aka KeyVectors

--- src/embeddings.py
+++ src/embeddings.py
@@ -9,17 +9,134 @@ import pandas as pd
 import time
 import warnings
 import csrgraph as cg
-from nodevectors.embedders import BaseNodeEmbedder
+import sklearn
+from sklearn.base import BaseEstimator
+import tempfile
+import joblib
+import sys
+import json
+import os
+import shutil
 import scipy
 from scipy import sparse, linalg
 from sklearn import preprocessing
 from sklearn.utils.extmath import randomized_svd
+from gensim.models import KeyedVectors
 
 # Gensim triggers automatic useless warnings for windows users...
 warnings.simplefilter("ignore", category=UserWarning)
 import gensim
 warnings.simplefilter("default", category=UserWarning)
 
+
+
+class BaseNodeEmbedder(BaseEstimator):
+    """
+    Base Class for node vector embedders
+    Includes saving, loading, and predict_new methods
+    """
+    f_model = "model.pckl"
+    f_mdata = "metadata.json"
+
+    def save(self, filename: str):
+        """
+        Saves model to a custom file format
+        
+        filename : str
+            Name of file to save. Don't include filename extensions
+            Extensions are added automatically
+        
+        File format is a zipfile with joblib dump (pickle-like) + dependency metata
+        Metadata is checked on load.
+        
+        Includes validation and metadata to avoid Pickle deserialization gotchas
+        See here Alex Gaynor PyCon 2014 talk "Pickles are for Delis"
+            for more info on why we introduce this additional check
+        """
+        if '.zip' in filename:
+            raise UserWarning("The file extension '.zip' is automatically added"
+                + " to saved models. The name will have redundant extensions")
+        sysverinfo = sys.version_info
+        meta_data = {
+            "python_": f'{sysverinfo[0]}.{sysverinfo[1]}',
+            "skl_": sklearn.__version__[:-2],
+            "pd_": pd.__version__[:-2],
+            "csrg_": cg.__version__[:-2]
+        }
+        with tempfile.TemporaryDirectory() as temp_dir:
+            joblib.dump(self, os.path.join(temp_dir, self.f_model), compress=True)
+            with open(os.path.join(temp_dir, self.f_mdata), 'w') as f:
+                json.dump(meta_data, f)
+            filename = shutil.make_archive(filename, 'zip', temp_dir)
+
+    @staticmethod
+    def load(filename: str):
+        """
+        Load model from NodeEmbedding model zip file.
+        
+        filename : str
+            full filename of file to load (including extensions)
+            The file should be the result of a `save()` call
+            
+        Loading checks for metadata and raises warnings if pkg versions
+        are different than they were when saving the model.
+        """
+        with tempfile.TemporaryDirectory() as temp_dir:
+            shutil.unpack_archive(filename, temp_dir, 'zip')
+            model = joblib.load(os.path.join(temp_dir, BaseNodeEmbedder.f_model))
+            with open(os.path.join(temp_dir, BaseNodeEmbedder.f_mdata)) as f:
+                meta_data = json.load(f)
+            # Validate the metadata
+            sysverinfo = sys.version_info
+            pyver = "{0}.{1}".format(sysverinfo[0], sysverinfo[1])
+            if meta_data["python_"] != pyver:
+                raise UserWarning(
+                    "Invalid python version; {0}, required: {1}".format(
+                        pyver, meta_data["python_"]))
+            sklver = sklearn.__version__[:-2]
+            if meta_data["skl_"] != sklver:
+                raise UserWarning(
+                    "Invalid sklearn version; {0}, required: {1}".format(
+                        sklver, meta_data["skl_"]))
+            pdver = pd.__version__[:-2]
+            if meta_data["pd_"] != pdver:
+                raise UserWarning(
+                    "Invalid pandas version; {0}, required: {1}".format(
+                        pdver, meta_data["pd_"]))
+            csrv = cg.__version__[:-2]
+            if meta_data["csrg_"] != csrv:
+                raise UserWarning(
+                    "Invalid csrgraph version; {0}, required: {1}".format(
+                        csrv, meta_data["csrg_"]))
+        return model
+
+    def predict(self, node_name):
+        """
+        Return vector associated with node
+        node_name : str or int
+            either the node ID or node name depending on graph format
+        """
+        return self.model[node_name]
+
+    def predict_new(self, 
+            neighbor_list,
+            pooling=lambda x : np.mean(x, axis=0)):
+        """
+        Predicts a new node from its neighbors
+        Generally done through the average of the embeddings
+            of its neighboring nodes.
+            
+        neighbor_list : iterable[node_id or node name]
+            list of node names/IDs with edges to the new node to be predicted
+        
+        pooling : function array[ndim] -> array[1d]
+            function to reduce the embeddings. Normally np.mean or np.sum
+            This takes the embeddings of the neighbors and reduces them 
+               to the final estimate for the unseen node
+
+        TODO: test
+        """
+        return pooling([self.predict(x) for x in neighbor_list])
 
 class Node2Vec(BaseNodeEmbedder):
     def __init__(
@@ -365,6 +482,29 @@ class ProNE(BaseNodeEmbedder):
         emb = ProNE.svd_dense(mm, n_components)
         return emb
     
+    @staticmethod
+    def save_vectors(instance, save_path):
+        """
+        Save node vectors in Word2Vec format.
+
+        Parameters
+        ----------
+        instance : ProNE
+            An instance of the ProNE model containing the node vectors
+        save_path : str
+            Path where the Word2Vec file will be saved
+        """
+        # Extract node IDs and vectors directly from the instance
+        node_ids = list(map(str, instance.model.keys()))  # Convert node IDs to strings
+        vectors = list(instance.model.values())  # Extract node vectors
+
+        # Create KeyedVectors instance and add vectors
+        kv = KeyedVectors(vector_size=len(vectors[0]))
+        kv.add_vectors(node_ids, vectors)
+
+        # Save in Word2Vec format
+        kv.save_word2vec_format(save_path, binary=True)
+    
 
 class GGVec(BaseNodeEmbedder):
     def __init__(self, 
@@ -483,7 +623,30 @@ class GGVec(BaseNodeEmbedder):
             verbose=self.verbose)
         self.model = dict(zip(G.nodes(), vectors))
         return vectors
+  
+    @staticmethod
+    def save_vectors(instance, save_path):
+        """
+        Save node vectors in Word2Vec format.
 
+        Parameters
+        ----------
+        instance : GGVec
+            An instance of the GGVec model containing the node vectors
+        save_path : str
+            Path where the Word2Vec file will be saved
+        """
+        # Extract node IDs and vectors directly from the instance
+        node_ids = list(map(str, instance.model.keys()))  # Convert node IDs to strings
+        vectors = list(instance.model.values())  # Extract node vectors
+
+        # Create KeyedVectors instance and add vectors
+        kv = KeyedVectors(vector_size=len(vectors[0]))
+        kv.add_vectors(node_ids, vectors)
+
+        # Save in Word2Vec format
+        kv.save_word2vec_format(save_path, binary=True)
+        
 
 def simple_node_embedding(G, dim=64):
     print("Generating simple node embeddings...")
