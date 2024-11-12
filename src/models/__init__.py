11907d925ba7247bfddd044d5a13cbeb08755fb2
ahmedsalim <realahmedsalim@gmail.com>
2024-11-12 16:50:38 +0800

    fixed typo

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from .models_dict import sklearn_models
 from .model_training import train_model, validate_model
-from tensorflow_models import train_tf_model
+from .tensorflow_models import train_tf_model
 from .model_evaluation import ModelEvaluation
